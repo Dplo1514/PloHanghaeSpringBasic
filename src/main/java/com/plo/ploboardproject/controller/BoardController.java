6e799a9be471e7bb9603ff701fea55351e191ffd
Dplo1514 <dladlsgur3334@gmail.com>
2022-03-30 10:58:53 +0900

    board final

--- src/main/java/com/plo/ploboardproject/controller/BoardController.java
+++ src/main/java/com/plo/ploboardproject/controller/BoardController.java
@@ -38,10 +38,10 @@ public class BoardController {
     }
 
     @GetMapping("/postPage")
-    public String boardWrite(Principal principal){
+    public String boardWrite(Principal principal) {
         try {
             principal.getName();
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             return "redirect:/";
         }
         return "board_write";
@@ -50,14 +50,14 @@ public class BoardController {
 
     @PostMapping("/post")
     public String boardWrite(@RequestParam(name = "title") String title,
-                             @RequestParam(name = "contents") String contents ,
-                             Principal principal,Model model){
+                             @RequestParam(name = "contents") String contents,
+                             Principal principal, Model model) {
         String name = principal.getName();
         String titles = title;
         String content = contents;
         try {
-            boardService.BoardSave( name ,titles , content);
-        } catch (IllegalArgumentException e){
+            boardService.BoardSave(name, titles, content);
+        } catch (IllegalArgumentException e) {
             String warning = "게시글 내용 및 제목을 입력해주세요";
             model.addAttribute("warning", warning);
             return "board_write";
@@ -66,15 +66,15 @@ public class BoardController {
     }
 
     @GetMapping("/boardView")
-    public String boardView(@RequestParam(value = "idx" , defaultValue = "0") long id ,Model model, Principal principal) {
+    public String boardView(@RequestParam(value = "idx", defaultValue = "0") long id, Model model, Principal principal) {
         Board getBoard = boardRepository.getById(id);
         List<Comment> getComment = commentRepository.findByBoard_idOrderByModifiedAtDesc(id);
         try {
             String user = principal.getName();
-            model.addAttribute("user" , user);
+            model.addAttribute("user", user);
             model.addAttribute("li", getBoard);
             model.addAttribute("comment", getComment);
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             model.addAttribute("li", getBoard);
             model.addAttribute("comment", getComment);
         }
@@ -83,16 +83,16 @@ public class BoardController {
 
 
     @PostMapping("/boardView")
-    public String commentPost(@RequestParam(value = "idx" , defaultValue = "0") long idx , @RequestParam(name = "comment") String comment , Principal principal, RedirectAttributes redirectAttributes) {
+    public String commentPost(@RequestParam(value = "idx", defaultValue = "0") long idx, @RequestParam(name = "comment") String comment, Principal principal, RedirectAttributes redirectAttributes) {
         String comments = comment;
         try {
             String name = principal.getName();
             boardService.CommentSave(name, comments, idx);
-        }   catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             String warning = "로그인 후 이용 가능한 기능입니다";
             redirectAttributes.addFlashAttribute("warning", warning);
             return "redirect:/board/boardView?idx=" + idx;
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             String warning = "댓글 내용을 입력해주세요";
             redirectAttributes.addFlashAttribute("warning", warning);
             return "redirect:/board/boardView?idx=" + idx;
@@ -102,13 +102,13 @@ public class BoardController {
 
     @ResponseBody
     @RequestMapping("/userCheck")
-    public Map<String , String> userCheck (Principal principal){
-        Map<String , String> result = new HashMap<String , String>();
+    public Map<String, String> userCheck(Principal principal) {
+        Map<String, String> result = new HashMap<String, String>();
         try {
             String userCheck = principal.getName();
-            result.put("user" , userCheck);
-        }catch (NullPointerException e){
-            result.put("user" , null);
+            result.put("user", userCheck);
+        } catch (NullPointerException e) {
+            result.put("user", null);
             return result;
         }
         System.out.println(result);
@@ -116,22 +116,26 @@ public class BoardController {
     }
 
     @PostMapping("/commentUpdate/{idx}")
-    public String commentUpdate(@PathVariable long idx ,
+    public String commentUpdate(@PathVariable long idx,
                                 @RequestBody HashMap<String, Object> map) {
         String comment = (String) map.get("commentUpdate");
         String commentId = (String) map.get("commentId");
         long id = Long.parseLong(commentId);
-        boardService.update(id , comment);
+        boardService.update(id, comment);
 
         return "redirect:/board/boardView?idx=" + idx;
     }
 
+    @RequestMapping("/commentDelete/{idx}")
+    public String commentDelete(@PathVariable long idx,
+                                @RequestBody HashMap<String, Object> map) {
+        String commentId = (String) map.get("commentIdx");
+        long id = Long.parseLong(commentId);
+        System.out.println(id);
+        commentRepository.deleteById(id);
+        return "redirect:/board/boardView?idx=" + idx;
+    }
 }
-
-
-
-
-
 
 
 
